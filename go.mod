2b917bea37fbc91d8ec302ecfb5d8f012364ca04
hytkgami <gagagatrump@gmail.com>
2023-04-22 23:56:56 +0900

    :white_check_mark: refactor: boot server, pass tests

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hytkgami/log-group-sample-go
 
 go 1.20
+
+require golang.org/x/sync v0.1.0 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/hytkgami/log-group-sample-go/controllers"
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
+	if err := run(context.Background()); err != nil {
+		log.Fatalf("failed to run: %+v", err)
+	}
+}
+
+func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -17,6 +29,24 @@ func main() {
 	router := http.NewServeMux()
 	healthCheckController := controllers.NewHealthCheckController()
 	router.HandleFunc("/ping", healthCheckController.Ping)
-	log.Println("Listening on port", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
+	s := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: router,
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		log.Println("Listening on port", port)
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	return eg.Wait()
 }
